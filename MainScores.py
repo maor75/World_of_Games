fd308cbb4a6b16190c63e8b23ea1bf404fcc6cd8
maor75 <129369594+maor75@users.noreply.github.com>
2023-05-31 17:17:59 +0300

    Add files via upload

---
+++ MainScores.py
@@ -0,0 +1,44 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def read_score():
+    try:
+        with open("Scores.txt", "r") as file:
+            score = int(file.read())
+        return score
+    except FileNotFoundError:
+        return "Score file not found"
+    except Exception as e:
+        return str(e)
+
+
+@app.route('/')
+def score_server():
+    score = read_score()
+    if isinstance(score, int):
+        return f'''
+        <html>
+        <head>
+        <title>Scores Game</title>
+        </head>
+        <body>
+        <h1>The score is <div id="score">{score}</div></h1>
+        </body>
+        </html>
+        '''
+    else:
+        return f'''
+        <html>
+        <head>
+        <title>Scores Game</title>
+        </head>
+        <body>
+        <h1><div id="score" style="color:red">{score}</div></h1>
+        </body>
+        </html>
+        '''
+
+
+app.run(host="0.0.0.0", port=5001, debug=False)
